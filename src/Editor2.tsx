faa9ea7f342072755d6a73a61670d6833863c426
SnO2WMaN <me@sno2wman.net>
2022-02-24 04:24:09 +0900

    tokenize simply

--- src/Editor2.tsx
+++ src/Editor2.tsx
@@ -4,8 +4,12 @@ import { BaseEditor, createEditor, Descendant, NodeEntry, Range, Text } from "sl
 import { withHistory } from "slate-history";
 import { Editable, ReactEditor, RenderElementProps, RenderLeafProps, Slate, withReact } from "slate-react";
 
+type Decorate = "bold" | "italic" | "monospace" | "del" | "wavy";
+
 type CustomElement = { type: "paragraph"; children: CustomText[]; };
-type CustomText = { text: string; } & { bold?: true; };
+type CustomText =
+  & { text: string; }
+  & { [key in Decorate]?: true; };
 
 declare module "slate" {
   interface CustomTypes {
@@ -21,12 +25,41 @@ const Leaf = ({ attributes, children, leaf }: RenderLeafProps) => {
       {...attributes}
       className={cx(
         leaf.bold && css({ fontWeight: "bold" }),
-        leaf.bold && css({ fontWeight: "bold" }),
+        leaf.italic && css({ fontStyle: "italic" }),
+        leaf.monospace && css({ fontFamily: "monospace" }),
+        leaf.del && css({ textDecoration: "line-through" }),
+        leaf.wavy && css({ textDecoration: "wavy underline" }),
       )}
     >
       {children}
     </span>
   );
+};
+
+export const tokenize = (
+  text: string,
+  offset = 0,
+) => {
+  const tokens: { type: Decorate; start: number; end: number; }[] = [];
+
+  const simpleDecorate = (pattern: string, type: Decorate) => {
+    const result = new RegExp(pattern, "d").exec(text);
+    if (result) {
+      const indices: [number, number] = (result as any).indices[0];
+      tokens.push({ type, start: offset + indices[0], end: offset + indices[1] });
+
+      // tokens.push(...tokenize(text.slice(0, indices[0]), offset, accepts.filter((v) => v !== type)));
+      tokens.push(...tokenize(text.slice(indices[1]), offset + indices[1]));
+    }
+  };
+
+  simpleDecorate("\\`[^\\`]+?\\`", "monospace");
+  simpleDecorate("\\*[^\\*]+?\\*", "bold");
+  simpleDecorate("\\_[^\\_]+?\\_", "italic");
+  simpleDecorate("\\-[^\\-]+?\\-", "del");
+  simpleDecorate("\\~[^\\~]+?\\~", "wavy");
+
+  return tokens;
 };
 
 export const Editor2: React.VFC<{ lines: { id: string; text: string; }[]; }> = ({ lines }) => {
@@ -35,10 +68,19 @@ export const Editor2: React.VFC<{ lines: { id: string; text: string; }[]; }> = (
     lines.map(({ text }) => ({ type: "paragraph", children: [{ text }] })),
   );
 
+  const contextMap = useMemo(() => {
+    console.dir(value);
+    return new Map();
+  }, [value]);
+
   const decorate = useCallback(([node, path]: NodeEntry) => {
     const ranges: Range[] = [];
 
     if (!Text.isText(node)) return ranges;
+    const tokens = tokenize(node.text);
+    for (const token of tokens) {
+      ranges.push({ [token.type]: true, anchor: { path, offset: token.start }, focus: { path, offset: token.end } });
+    }
 
     return ranges;
   }, []);
